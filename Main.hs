25d5a69459eb181e82226c664499c23d928f7da6
Elif Ak <akeli@itu.edu.tr>
2018-04-22 15:44:59 +0300

    Type declarations and wordCharCounts function
    First draft of 'wordCharCounts' is implemented using map, filter, zip, toLower and nub functions.

---
+++ Main.hs
@@ -0,0 +1,13 @@
+import Prelude hiding (Word)
+import Data.Char
+import Data.List
+
+type Word = String
+type Sentence = [Word]
+type CharCount = [(Char, Int)]
+
+wordCharCounts :: Word -> CharCount
+wordCharCounts cs = nub (zip cs' ns)
+    where
+        ns  = map (\x -> length (filter (==x) cs')) cs'
+        cs' = map toLower cs
