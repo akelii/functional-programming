b4f12031898250a5ff2e36102e317f167ecd1ea1
Elif Ak <akeli@itu.edu.tr>
2018-04-29 21:47:13 +0300

    wordCharCounts is updated
    The result of the wordCharCounts is updated as sorted form. The result is sorted according to first character of tuple (alphabetic ones). By this way, we can obtain same words which have same character count.

--- Main.hs
+++ Main.hs
@@ -1,16 +1,23 @@
 import Prelude hiding (Word)
 import Data.Char
 import Data.List
+import Data.Ord
+import System.IO
 
 type Word = String
 type Sentence = [Word]
 type CharCount = [(Char, Int)]
 
+{-
+cmp function is implemented using the link below (for usage of fst and snd):
+stackoverflow.com/questions/2349798/in-haskell-how-can-i-use-the-built-in-sortby-function-to-sort-a-list-of-pairst/2352333#2352333
+-}
 wordCharCounts :: Word -> CharCount
-wordCharCounts cs = nub (zip cs' ns)
+wordCharCounts cs = sortBy cmp (nub $ zip cs' ns)
     where
         ns  = map (\x -> length (filter (==x) cs')) cs'
         cs' = map toLower cs
+        cmp = comparing fst
 
 sentenceCharCounts :: Sentence -> CharCount
 sentenceCharCounts = wordCharCounts . concat
