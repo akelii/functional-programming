d46e45f3d6e4bfd22d4e3efa7d5fedcaee2837da
Elif Ak <akeli@itu.edu.tr>
2018-05-20 14:20:08 +0300

    update on insert
    insert function is re-implemented to change "end flag" logic. "end flag" is set for the character placed in the before the "end flag". For example the "end flag" for the word "an" is set like following:
    Trie {end = False, children = fromList [('a',Trie {end = False, children = fromList [('n',Trie {end = True, children = fromList []})]}
    
    But in before implementation, "end flag" is set for the character placed in the after the "end flag". Like following:
    Trie {end = False, children = fromList [('a',Trie {end = True, children = fromList [('n',Trie {end = True, children = fromList []})]}
    So this leads to confusings for end of the word in some cases. This bug is fixed.

--- trie.hs
+++ trie.hs
@@ -9,13 +9,13 @@ data Trie = Trie {end :: Bool, children :: Map.Map Char Trie} deriving (Show, Eq
 type Word = String
 
 empty :: Trie
-empty = Trie {end = True, children = Map.empty}
+empty = Trie {end = False, children = Map.empty}
 
 insert :: Word -> Trie -> Trie
 insert [] _ = empty
 insert (x:xs) t
-    | xs == []                               = Trie {end = True, children = Map.insert x (fromMaybe empty $ Map.lookup x (children t)) $ children t}
-    | (Map.lookup x $ children t) == Nothing = Trie {end = False, children = Map.insert x (insert xs (fromMaybe empty $ Map.lookup x (children t))) (children t)} 
+    | xs == []                               = Trie {end = (end t), children = Map.insert x (Trie {end=True, children = children (fromMaybe empty $ Map.lookup x (children t))}) (children t)}
+    | (Map.lookup x $ children t) == Nothing = Trie {end = (end t), children = Map.insert x (insert xs (fromMaybe empty $ Map.lookup x (children t))) (children t)}
     | otherwise                              = Trie {end = (end t), children = Map.insert x (insert xs $ fromJust (Map.lookup x $ children t)) (children t)}
 
 insertList :: [Word] -> Trie
