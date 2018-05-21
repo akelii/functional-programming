39819428676b0f78418785e25f7ffcaa8fc2c40a
Elif Ak <akeli@itu.edu.tr>
2018-05-22 01:14:10 +0300

    doAction is implemented
    doAction is implemented to take Trie and produce IO type in recursion way.

--- trie.hs
+++ trie.hs
@@ -66,3 +66,31 @@ getInput = do
         else do putStrLn "Enter word/prefix:"
                 w <- getLine
                 return (convertAction $ c!!0, w)
+
+doAction :: Trie -> IO ()
+doAction t = do
+    (a,w) <- getInput
+    if a == Find
+        then do if (fromMaybe [] $ prefix w t) == [] then putStrLn "No words found with that prefix!" else putStrLn ("Found words:\n" ++ (unlines $ fromMaybe [] $ prefix w t))
+                doAction t
+        else if a == Add
+            then do putStrLn "New word is added!"
+                    doAction (insert w t)
+            else if a == Search
+                then do if (search w t) == True then putStrLn "Exists in dictionary!" else putStrLn "NOT exist!"
+                        doAction t
+                else if a == Print
+                    then do putStrLn "List of words in dictionary:"
+                            let result = unlines $ getWords t
+                            putStrLn result
+                            doAction t
+                    else if a == Exit
+                        then do putStrLn "Good bye dear user!"
+                                return ()
+                        else do putStrLn "Invalid action!"
+                                menu <- getMenu
+                                putStrLn menu
+                                doAction t
+
+getMenu :: IO String
+getMenu = return "a) Add Word\ns) Search Word\nf) Find words with prefix\np) Print all words\ne) Exit"
